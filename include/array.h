35c1d2c82146f3dbf7f6d502ad2fc2a1ba45a53d
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-07-17 23:15:52 +0700

    Define array struct and basic array function -------------------------------------------- Basic array function defined:  - new array function  - sum of array function

--- include/array.h
+++ include/array.h
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_ARRAY_H
 #define INCLUDE_ARRAY_H
 
+typedef struct Array2D
+{
+	int row;
+	int col;
+
+	int** data;
+}Array2D;
+
+int new2Darray(int row, int col, Array2D* newArr);
+int sumArray2D(Array2D arr, int** result);
+
 #endif
