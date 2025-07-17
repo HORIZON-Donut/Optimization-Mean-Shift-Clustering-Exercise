9210cf440855db375788864b931786895990d5d8
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-07-17 23:20:50 +0700

    Implement new array function

--- src/array.c
+++ src/array.c
@@ -1,7 +1,13 @@
 #include "array.h"
+#include <stdlib.h>
 
 int new2Darray(int row, int col, Array2D* newArr)
 {
+	newArr->row = row;
+	newArr->col = col;
+
+	newArr->data = (int**)malloc(sizeof(int) * row * col);
+
 	return 0;
 }
 int sumArray2D(Array2D arr, int** result)
