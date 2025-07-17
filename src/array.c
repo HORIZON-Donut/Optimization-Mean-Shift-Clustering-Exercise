b2eaf64c939d4b1b25c82d15293290bcf014bdd0
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-07-17 23:33:13 +0700

    Implement sum of array

--- src/array.c
+++ src/array.c
@@ -10,8 +10,22 @@ int new2Darray(int row, int col, Array2D* newArr)
 
 	return 0;
 }
-int sumArray2D(Array2D arr, int** result)
+
+int sumArray2D(Array2D* arr, int* result)
 {
+	int* tmp = (int*)malloc(sizeof(int) * arr->col);
+
+	for(int i = 0; i < arr->row; i++)
+	{
+		for(int j = 0; j < arr->col; j++)
+		{
+			tmp[j] = tmp[j] + arr->data[i][j];
+		}
+	}
+
+	result = tmp;
+	free(tmp);
+
 	return 0;
 }
 
