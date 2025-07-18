22e0d358ff5b667c040bc6c140c94d3ae875c67f
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-07-18 17:43:38 +0700

    Fixed wrong function call

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def meanShift(X, data):
 	return resultX - X[0], resultY - X[1]
 
 def distance(X, Y):
-	return math.sprt((X[0] - Y[0])**2 + (X[1] - Y[1])**2)
+	return math.sqrt((X[0] - Y[0])**2 + (X[1] - Y[1])**2)
 
 def main():
 	data = [
